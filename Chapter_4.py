57654dfd80c5fdbce4bb27a8b0eabcb69c014ece
Carson <56667132+carsonforeman@users.noreply.github.com>
2021-08-02 11:55:02 -0500

    Update Chapter_4.py

--- Chapter_4.py
+++ Chapter_4.py
@@ -111,18 +111,4 @@ print ("Thank you, everyone. That was a great magic show!")
 	#dimensions = (200,50)
 	#dimensions = (400, 100)
 
-#4-13. Buffet: A buffet-style restaurant offers only five basic foods. Think of five
-#simple foods, and store them in a tuple.
-#Use a for loop to print each food the restaurant offers.
-#Try to modify one of the items, and make sure that Python rejects the
-#change.
-#The restaurant changes its menu, replacing two of the items with different
-#foods. Add a line that rewrites the tuple, and then use a for loop to print
-#each of the items on the revised menu.
 
-buffet_foods = ('eggs', 'bacon', 'sausage', 'pancakes', 'oatmeal')
-for food in buffet_foods:
-	print (food)
-buffet_foods = ('eggs', 'bacon','sausage','waffles','grits')
-for food in buffet_foods:
-	print (food)
